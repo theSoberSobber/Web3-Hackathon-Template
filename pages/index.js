34af75d74e12128d6eff5131d63a6dcf378b922c
theSoberSobber <akshatshrivastava0010@gmail.com>
2022-10-17 18:05:41 +0530

    login

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import styles from "../styles/Home.module.css";
+export default function Home() {
+    return (
+      <div className={styles.container}>
+        Billi Meow Meow kyu karti hai? <br />
+        https://www.bbc.com/hindi/vert-fut-44985200
+      </div>
+    );
+  }
