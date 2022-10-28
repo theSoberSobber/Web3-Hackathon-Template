d0fb3e1ddae92630b1255ffdb4810f7166059822
theSoberSobber <akshatshrivastava0010@gmail.com>
2022-10-28 21:44:21 +0530

    changes

--- .history/pages/index_20221017175908.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-      <div className={styles.container}>
-        Billi Meow Meow kyu karti hai? <br />
-        https://www.bbc.com/hindi/vert-fut-44985200
-      </div>
-    );
-  }
--- .history/pages/index_20221017180422.js
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/Home.module.css";
-export default function Home() {
-    return (
-      <div className={styles.container}>
-        Billi Meow Meow kyu karti hai? <br />
-        https://www.bbc.com/hindi/vert-fut-44985200
-      </div>
-    );
-  }
